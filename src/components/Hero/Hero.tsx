150fa3b3bed3b69b3a507f02451f866331b6d72b
Hunter Parcells <hunterparcells@gmail.com>
2023-03-07 01:12:34 -0500

    Hero banner iamge.

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,10 +1,15 @@
 import classes from './Hero.module.scss';
+import Image from 'next/image';
+
+import heroImage from '../../../public/hero.png';
 
 function Hero() {
   return (
     <div className={classes.root}>
       <p className={classes.big}>Hero</p>
       <p className={classes.light}>Hero</p>
+
+      <Image src={heroImage} alt='Banner' fill className={classes.image} />
     </div>
   );
 }
