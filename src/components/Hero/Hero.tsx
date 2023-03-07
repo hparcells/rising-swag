3c2836e46900179138b3d9e35e2f40e32be6b27b
Hunter Parcells <hunterparcells@gmail.com>
2023-03-07 11:12:21 -0500

    Hero content.

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,8 +6,11 @@ import heroImage from '../../../public/hero.png';
 function Hero() {
   return (
     <div className={classes.root}>
-      <p className={classes.big}>Hero</p>
-      <p className={classes.light}>Hero</p>
+      <p className={classes.big}>Red Rising Merch</p>
+      <p className={classes.light}>
+        Represent the world of Pierce Brown’s Red Rising with style with a curated list of merch and
+        other Red Rising related items from members across the community, all in one place.
+      </p>
 
       <Image src={heroImage} alt='Banner' fill className={classes.image} />
     </div>
