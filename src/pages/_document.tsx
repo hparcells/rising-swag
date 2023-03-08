ce9f4b195965b3ceea48b63b2d32b39bfe8bb640
Hunter Parcells <hunterparcells@gmail.com>
2023-03-08 00:20:09 -0500

    Added favicons and meta tags.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,45 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 const DESCRIPTION =
-  'Portfolio website for Hunter Parcells, a computer science student and aspiring web developer out of southeast Michigan. Be sure to check out The Archives for more!';
+  "Represent the world of Pierce Brown's Red Rising with style with a curated list of merch and other Red Rising related items from members across the community, all in one place.";
 
 function Document() {
   return (
     <Html>
-      <Head>{/* TODO: All the meta tags. */}</Head>
+      <Head>
+        <meta charSet='UTF-8' />
+        <meta name='language' content='EN' />
+        <meta name='keywords' content='red,rising,bookish,merch,stores,small,business,pin,pins' />
+        <meta name='author' content='Hunter Parcells' />
+
+        <meta name='description' content={DESCRIPTION} />
+        <meta property='og:description' content={DESCRIPTION} />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://redrising.hunterparcells.com/' />
+        <meta property='og:image' content='/icon/favicon-310.png' />
+        <meta name='theme-color' content='#FD252F' />
+
+        <link rel='shortcut icon' href='/icon/favicon.ico' />
+        <link rel='icon' sizes='16x16 32x32 64x64' href='/icon/favicon.ico' />
+        <link rel='icon' type='image/png' sizes='196x196' href='/icon/favicon-192.png' />
+        <link rel='icon' type='image/png' sizes='160x160' href='/icon/favicon-160.png' />
+        <link rel='icon' type='image/png' sizes='96x96' href='/icon/favicon-96.png' />
+        <link rel='icon' type='image/png' sizes='64x64' href='/icon/favicon-64.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/icon/favicon-32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/icon/favicon-16.png' />
+        <link rel='apple-touch-icon' href='/icon/favicon-57.png' />
+        <link rel='apple-touch-icon' sizes='114x114' href='/icon/favicon-114.png' />
+        <link rel='apple-touch-icon' sizes='72x72' href='/icon/favicon-72.png' />
+        <link rel='apple-touch-icon' sizes='144x144' href='/icon/favicon-144.png' />
+        <link rel='apple-touch-icon' sizes='60x60' href='/icon/favicon-60.png' />
+        <link rel='apple-touch-icon' sizes='120x120' href='/icon/favicon-120.png' />
+        <link rel='apple-touch-icon' sizes='76x76' href='/icon/favicon-76.png' />
+        <link rel='apple-touch-icon' sizes='152x152' href='/icon/favicon-152.png' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/icon/favicon-180.png' />
+        <meta name='msapplication-TileColor' content='#C20116' />
+        <meta name='msapplication-TileImage' content='/icon/favicon-144.png' />
+        <meta name='msapplication-config' content='/icon/browserconfig.xml' />
+      </Head>
       <body>
         <Main />
         <NextScript />
