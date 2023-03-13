9ef02c3b5b6124d8eeb9e3c876f2e972aa18a678
Hunter Parcells <hunterparcells@gmail.com>
2023-03-13 14:09:11 -0400

    Added hats from Lit Escalates. Finished Lit Escalates items.

--- src/data/hats.ts
+++ src/data/hats.ts
@@ -42,6 +42,189 @@ const data: IItem[] = [
     description:
       "You asked and we delivered: A snug, form-fitting beanie embroidered with the Howler Wolf. Available in black, grey and white, our beanie will keep your dome warmer than Volga's scorcher.",
     link: 'https://www.lit-escalates.com/collections/apparel/products/embroidered-howler-beanie'
+  },
+  {
+    date: '2023-03-13',
+    image:
+      'https://cdn.shopify.com/s/files/1/0374/1499/4053/products/retro-trucker-hat-black-front-60a698bf0bdea_1024x1024.jpg',
+    name: 'Gold Sigil Trucker Cap',
+    tags: ['hat', 'gold', 'official'],
+    shop: {
+      name: 'Lit Escalates',
+      url: 'https://www.lit-escalates.com/'
+    },
+    description: "Emblazoned with the Official Gold Sigil from Pierce Brown's Red Rising series.",
+    link: 'https://www.lit-escalates.com/collections/apparel/products/gold-sigil-trucker-cap'
+  },
+  {
+    date: '2023-03-13',
+    image:
+      'https://cdn.shopify.com/s/files/1/0374/1499/4053/products/retro-trucker-hat-black-front-60a698886890f_1024x1024.jpg',
+    name: 'Red Sigil Trucker Cap',
+    tags: ['hat', 'red', 'official'],
+    shop: {
+      name: 'Lit Escalates',
+      url: 'https://www.lit-escalates.com/'
+    },
+    description: "Emblazoned with the Official Red Sigil from Pierce Brown's Red Rising series.",
+    link: 'https://www.lit-escalates.com/collections/apparel/products/red-sigil-trucker-cap'
+  },
+  {
+    date: '2023-03-13',
+    image:
+      'https://cdn.shopify.com/s/files/1/0374/1499/4053/products/retro-trucker-hat-black-front-60a699a455a34_1024x1024.jpg',
+    name: 'Obsidian Sigil Trucker Cap',
+    tags: ['hat', 'obsidian', 'official'],
+    shop: {
+      name: 'Lit Escalates',
+      url: 'https://www.lit-escalates.com/'
+    },
+    description:
+      "Emblazoned with the Official Obsidian Sigil from Pierce Brown's Red Rising series.",
+    link: 'https://www.lit-escalates.com/collections/apparel/products/obsidian-sigil-trucker-cap'
+  },
+  {
+    date: '2023-03-13',
+    image:
+      'https://cdn.shopify.com/s/files/1/0374/1499/4053/products/retro-trucker-hat-black-front-60a696a845b4f_1024x1024.jpg',
+    name: 'Violet Sigil Trucker Cap',
+    tags: ['hat', 'violet', 'official'],
+    shop: {
+      name: 'Lit Escalates',
+      url: 'https://www.lit-escalates.com/'
+    },
+    description: 'Emblazoned with the Official Violet Sigil from Pierce Brown’s Red Rising series.',
+    link: 'https://www.lit-escalates.com/collections/apparel/products/violet-sigil-trucker-cap'
+  },
+  {
+    date: '2023-03-13',
+    image:
+      'https://cdn.shopify.com/s/files/1/0374/1499/4053/products/retro-trucker-hat-black-front-60a695f4bcb26_1024x1024.jpg',
+    name: 'Gray Sigil Trucker Cap',
+    tags: ['hat', 'gray', 'official'],
+    shop: {
+      name: 'Lit Escalates',
+      url: 'https://www.lit-escalates.com/'
+    },
+    description: 'Emblazoned with the Official Gray Sigil from Pierce Brown’s Red Rising series.',
+    link: 'https://www.lit-escalates.com/collections/apparel/products/gray-sigil-trucker-cap'
+  },
+  {
+    date: '2023-03-13',
+    image:
+      'https://cdn.shopify.com/s/files/1/0374/1499/4053/products/retro-trucker-hat-black-front-60a6977cd7b87_1024x1024.jpg',
+    name: 'Yellow Sigil Trucker Cap',
+    tags: ['hat', 'yellow', 'official'],
+    shop: {
+      name: 'Lit Escalates',
+      url: 'https://www.lit-escalates.com/'
+    },
+    description: 'Emblazoned with the Official Yellow Sigil from Pierce Brown’s Red Rising series.',
+    link: 'https://www.lit-escalates.com/collections/apparel/products/yellow-sigil-trucker-cap'
+  },
+  {
+    date: '2023-03-13',
+    image:
+      'https://cdn.shopify.com/s/files/1/0374/1499/4053/products/retro-trucker-hat-black-front-60a697444ba0e_1024x1024.jpg',
+    name: 'Green Sigil Trucker Cap',
+    tags: ['hat', 'green', 'official'],
+    shop: {
+      name: 'Lit Escalates',
+      url: 'https://www.lit-escalates.com/'
+    },
+    description: 'Emblazoned with the Official Green Sigil from Pierce Brown’s Red Rising series.',
+    link: 'https://www.lit-escalates.com/collections/apparel/products/green-sigil-trucker-cap'
+  },
+  {
+    date: '2023-03-13',
+    image:
+      'https://cdn.shopify.com/s/files/1/0374/1499/4053/products/retro-trucker-hat-black-front-60a69624e480a_1024x1024.jpg',
+    name: 'Orange Sigil Trucker Cap',
+    tags: ['hat', 'orange', 'official'],
+    shop: {
+      name: 'Lit Escalates',
+      url: 'https://www.lit-escalates.com/'
+    },
+    description: 'Emblazoned with the Official Orange Sigil from Pierce Brown’s Red Rising series.',
+    link: 'https://www.lit-escalates.com/collections/apparel/products/orange-sigil-trucker-cap'
+  },
+  {
+    date: '2023-03-13',
+    image:
+      'https://cdn.shopify.com/s/files/1/0374/1499/4053/products/retro-trucker-hat-black-front-60a69a3dd5b2a_1024x1024.jpg',
+    name: 'Pink Sigil Trucker Cap',
+    tags: ['hat', 'pink', 'official'],
+    shop: {
+      name: 'Lit Escalates',
+      url: 'https://www.lit-escalates.com/'
+    },
+    description: 'Emblazoned with the Official Pink Sigil from Pierce Brown’s Red Rising series.',
+    link: 'https://www.lit-escalates.com/collections/apparel/products/pink-sigil-trucker-cap'
+  },
+  {
+    date: '2023-03-13',
+    image:
+      'https://cdn.shopify.com/s/files/1/0374/1499/4053/products/retro-trucker-hat-black-front-60a697ccf17f1_1024x1024.jpg',
+    name: 'Blue Sigil Trucker Cap',
+    tags: ['hat', 'blue', 'official'],
+    shop: {
+      name: 'Lit Escalates',
+      url: 'https://www.lit-escalates.com/'
+    },
+    description: 'Emblazoned with the Official Blue Sigil from Pierce Brown’s Red Rising series.',
+    link: 'https://www.lit-escalates.com/collections/apparel/products/blue-sigil-trucker-cap'
+  },
+  {
+    date: '2023-03-13',
+    image:
+      'https://cdn.shopify.com/s/files/1/0374/1499/4053/products/retro-trucker-hat-black-front-60a6985950b0f_1024x1024.jpg',
+    name: 'Silver Sigil Trucker Cap',
+    tags: ['hat', 'silver', 'official'],
+    shop: {
+      name: 'Lit Escalates',
+      url: 'https://www.lit-escalates.com/'
+    },
+    description: 'Emblazoned with the Official Silver Sigil from Pierce Brown’s Red Rising series.',
+    link: 'https://www.lit-escalates.com/collections/apparel/products/silver-sigil-trucker-cap'
+  },
+  {
+    date: '2023-03-13',
+    image:
+      'https://cdn.shopify.com/s/files/1/0374/1499/4053/products/retro-trucker-hat-black-front-60a697fd67c98_1024x1024.jpg',
+    name: 'Copper Sigil Trucker Cap',
+    tags: ['hat', 'copper', 'official'],
+    shop: {
+      name: 'Lit Escalates',
+      url: 'https://www.lit-escalates.com/'
+    },
+    description: 'Emblazoned with the Official Copper Sigil from Pierce Brown’s Red Rising series.',
+    link: 'https://www.lit-escalates.com/collections/apparel/products/copper-sigil-trucker-cap'
+  },
+  {
+    date: '2023-03-13',
+    image:
+      'https://cdn.shopify.com/s/files/1/0374/1499/4053/products/retro-trucker-hat-black-front-60a69824312af_1024x1024.jpg',
+    name: 'White Sigil Trucker Cap',
+    tags: ['hat', 'white', 'official'],
+    shop: {
+      name: 'Lit Escalates',
+      url: 'https://www.lit-escalates.com/'
+    },
+    description: 'Emblazoned with the Official White Sigil from Pierce Brown’s Red Rising series.',
+    link: 'https://www.lit-escalates.com/collections/apparel/products/white-sigil-trucker-cap'
+  },
+  {
+    date: '2023-03-13',
+    image:
+      'https://cdn.shopify.com/s/files/1/0374/1499/4053/products/retro-trucker-hat-black-front-60c8ad6717d75_1024x1024.jpg',
+    name: 'Brown Sigil Trucker Cap',
+    tags: ['hat', 'brown', 'official'],
+    shop: {
+      name: 'Lit Escalates',
+      url: 'https://www.lit-escalates.com/'
+    },
+    description: 'Emblazoned with the Official Brown Sigil from Pierce Brown’s Red Rising series.',
+    link: 'https://www.lit-escalates.com/collections/apparel/products/brown-sigil-trucker-cap'
   }
 ];
 
