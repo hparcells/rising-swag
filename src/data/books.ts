84f2ca921e002dbf8a09fde5e0401b1a911b40c3
Hunter Parcells <hunterparcells@gmail.com>
2023-03-29 22:58:56 -0400

    Added RareBiblio items.

--- src/data/books.ts
+++ src/data/books.ts
@@ -1,5 +1,19 @@
 import { IItem } from '@/types/item';
 
-const data: IItem[] = [];
+const data: IItem[] = [
+  {
+    date: '2023-03-29',
+    image: 'https://i.etsystatic.com/38199041/r/il/9e1191/4514119290/il_794xN.4514119290_tvz1.jpg',
+    name: 'Red Rising (Custom Leather Bound)',
+    tags: ['book'],
+    shop: {
+      name: 'RareBiblio',
+      url: 'https://www.etsy.com/shop/RareBiblio'
+    },
+    description:
+      'Custom leather bound copy of Red Rising, customizable with your choice of color of leather.',
+    link: 'https://www.etsy.com/listing/1359071248/red-rising-premium-leather-bound'
+  }
+];
 
 export default data;
