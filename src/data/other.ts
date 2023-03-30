c15cd685a87f410215553af2b6e7d2966d2ce376
Hunter Parcells <hunterparcells@gmail.com>
2023-03-29 22:56:18 -0400

    Added InTheBooksWeFindUs items.

--- src/data/other.ts
+++ src/data/other.ts
@@ -564,6 +564,18 @@ const data: IItem[] = [
     },
     description: "Red Rising candle from firenationcreations's Stuck in Space box.",
     link: 'https://www.etsy.com/listing/952004495/stuck-in-space-sci-fi-box-candles?variation0=1787670255'
+  },
+  {
+    date: '2023-03-29',
+    image: 'https://i.etsystatic.com/18861441/r/il/6e4cca/3412295655/il_794xN.3412295655_leco.jpg',
+    name: 'Red Rising Themed Candles',
+    tags: ['other', 'darrow', 'eo', 'sevro', 'mustang'],
+    shop: {
+      name: 'InTheBooksWeFindUs',
+      url: 'https://www.etsy.com/shop/InTheBooksWeFindUs'
+    },
+    description: 'Red Rising themed candles. Choose from Darrow, Eo, Sevro, or Mustang.',
+    link: 'https://www.etsy.com/listing/765659952/candele-a-tema-red-rising-2-oz'
   }
 ];
 
