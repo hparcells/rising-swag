47e185ff4f25193f3b258647354cc909c3640bce
Hunter Parcells <hunterparcells@gmail.com>
2023-04-06 22:58:53 -0400

    Readd Son of Ares items.

--- src/data/redbubble.ts
+++ src/data/redbubble.ts
@@ -1,5 +1,43 @@
 import { IItem } from '@/types/item';
 
-const data: IItem[] = [];
+const data: IItem[] = [
+  {
+    date: '2023-04-06',
+    image: 'https://ih1.redbubble.net/image.2266064843.0830/flat,600x600,075,f.jpg',
+    name: 'Omnis Vir Lupus Design',
+    tags: ['redbubble', 'red'],
+    shop: {
+      name: 'Son of Ares',
+      url: 'https://www.redbubble.com/people/Bitninjasupply/shop'
+    },
+    description: 'Every man a wolf. Omnis vir lupus design with a wolf and red sigil.',
+    link: 'https://www.redbubble.com/shop/ap/75140830'
+  },
+  {
+    date: '2023-04-06',
+    image: 'https://ih1.redbubble.net/image.2266050174.0427/flat,600x600,075,f.jpg',
+    name: 'The Reaper Design',
+    tags: ['redbubble', 'red', 'gold'],
+    shop: {
+      name: 'Son of Ares',
+      url: 'https://www.redbubble.com/people/Bitninjasupply/shop'
+    },
+    description: 'The Reaper design with a skull and gold and red sigils.',
+    link: 'https://www.redbubble.com/shop/ap/75140427'
+  },
+  {
+    date: '2023-04-06',
+    image: 'https://ih1.redbubble.net/image.1109768977.5905/flat,600x600,075,f.jpg',
+    name: 'Golden Howler Design',
+    tags: ['redbubble'],
+    shop: {
+      name: 'Son of Ares',
+      url: 'https://www.redbubble.com/people/Bitninjasupply/shop'
+    },
+    description:
+      "Become an initiated member of Sevro's Howlers by sporting this emblem on your favorite merch. Hail Reaper!",
+    link: 'https://www.redbubble.com/shop/ap/45915905'
+  }
+];
 
 export default data;
