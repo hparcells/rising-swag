2319a06c0a7490f05efc5b61f4c8a2583de69c48
Hunter Parcells <hunterparcells@gmail.com>
2023-04-24 13:04:19 -0400

    Added A Nerd Grows in Brooklyn items.

--- src/data/multiproduct/teepublic.ts
+++ src/data/multiproduct/teepublic.ts
@@ -13,6 +13,33 @@ const data: IItem[] = [
     },
     description: 'Helldiver of Lykos sketch.',
     link: 'https://www.teepublic.com/t-shirt/22690035-helldiver-of-lykos'
+  },
+  {
+    date: '2023-04-24',
+    image:
+      'https://res.cloudinary.com/teepublic/image/private/s--R5KdEwSM--/t_Preview/b_rgb:191919,c_limit,f_auto,h_630,q_90,w_630/v1638063496/production/designs/25899646_0.jpg',
+    name: 'Peerless Scarred Design',
+    tags: ['multiproduct'],
+    shop: {
+      name: 'A Nerd Grows in Brooklyn',
+      url: 'https://www.teepublic.com/user/a-nerd-grows-in-brooklyn'
+    },
+    description: 'The peerless scar on the face of a gold.',
+    link: 'https://www.teepublic.com/t-shirt/25899646-peerless-scarred'
+  },
+  {
+    date: '2023-04-24',
+    image:
+      'https://res.cloudinary.com/teepublic/image/private/s--qFfFx71F--/t_Preview/b_rgb:6e2229,c_limit,f_jpg,h_630,q_90,w_630/v1638067259/production/designs/25900396_0.jpg',
+    name: 'Ragnar Volarus: Shield of Tinos Design',
+    tags: ['multiproduct'],
+    shop: {
+      name: 'A Nerd Grows in Brooklyn',
+      url: 'https://www.teepublic.com/user/a-nerd-grows-in-brooklyn'
+    },
+    description:
+      "Ragnar's Coat of Arms is an obsidian background with streaks of gold running through it, and a red stripe across the center.",
+    link: 'https://www.teepublic.com/t-shirt/25900396-ragnar-volarus-shield-of-tinos'
   }
 ];
 
