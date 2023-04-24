4f9de625127ac3ff6c36e8d55168a707d50372a8
Hunter Parcells <hunterparcells@gmail.com>
2023-04-24 13:09:19 -0400

    Added SpiceDoc items.

--- src/data/multiproduct/teepublic.ts
+++ src/data/multiproduct/teepublic.ts
@@ -40,6 +40,45 @@ const data: IItem[] = [
     description:
       "Ragnar's Coat of Arms is an obsidian background with streaks of gold running through it, and a red stripe across the center.",
     link: 'https://www.teepublic.com/t-shirt/25900396-ragnar-volarus-shield-of-tinos'
+  },
+  {
+    date: '2023-04-24',
+    image:
+      'https://res.cloudinary.com/teepublic/image/private/s--gSSahchS--/t_Preview/b_rgb:191919,c_limit,f_auto,h_630,q_90,w_630/v1678401330/production/designs/40727353_0.jpg',
+    name: 'Bloodydamn Omnis Vir Lupus Design',
+    tags: ['multiproduct'],
+    shop: {
+      name: 'SpiceDoc',
+      url: 'https://www.teepublic.com/user/spicedoc'
+    },
+    description: 'Two of the most famous phrases from the Red Rising.',
+    link: 'https://www.teepublic.com/t-shirt/40727353-bloodydamn-red-rising-omnis-vir-lupus'
+  },
+  {
+    date: '2023-04-24',
+    image:
+      'https://res.cloudinary.com/teepublic/image/private/s--q-mmv9oz--/t_Preview/b_rgb:191919,c_limit,f_auto,h_630,q_90,w_630/v1678401328/production/designs/40727357_0.jpg',
+    name: 'Bloodydamn Omnis Vir Lupus Design (Red)',
+    tags: ['multiproduct'],
+    shop: {
+      name: 'SpiceDoc',
+      url: 'https://www.teepublic.com/user/spicedoc'
+    },
+    description: 'Two of the most famous phrases from the Red Rising.',
+    link: 'https://www.teepublic.com/t-shirt/40727357-bloodydamn-omnis-vir-lupus-v2-red-rising'
+  },
+  {
+    date: '2023-04-24',
+    image:
+      'https://res.cloudinary.com/teepublic/image/private/s--r6spvVEL--/t_Preview/b_rgb:191919,c_limit,f_auto,h_630,q_90,w_630/v1678401332/production/designs/40727358_0.jpg',
+    name: 'Omnis Vir Lupus The Howlers Design',
+    tags: ['multiproduct'],
+    shop: {
+      name: 'SpiceDoc',
+      url: 'https://www.teepublic.com/user/spicedoc'
+    },
+    description: 'A howling wolf and the phrase "Omnis Vir Lupus"',
+    link: 'https://www.teepublic.com/t-shirt/40727358-omnis-vir-lupus-the-howlers-red-rising'
   }
 ];
 
