961f803b669af40cc51b392fafb804d915d03c6a
Hunter Parcells <hunterparcells@gmail.com>
2023-04-25 01:14:08 -0400

    Added Wendyshop items.

---
+++ src/data/multiproduct/spreadshirt.ts
@@ -0,0 +1,19 @@
+import { IItem } from '@/types/item';
+
+const data: IItem[] = [
+  {
+    date: '2023-04-25',
+    image:
+      'https://image.spreadshirtmedia.com/image-server/v1/compositions/T812A1PA4267PT17X45Y87D1031409899W21161H18515/views/1,width=800,height=800,appearanceId=1,backgroundColor=FFFFFF,noPt=true/red-rising-mens-premium-t-shirt.jpg',
+    name: 'The Reaper of Mars Design',
+    tags: ['multiproduct', 'darrow'],
+    shop: {
+      name: 'Wendyshop',
+      url: 'https://www.spreadshirt.com/shop/user/wendyshop/'
+    },
+    description: '"The Reaper of Mars" with a slingBlade.',
+    link: 'https://www.spreadshirt.com/shop/design/red+rising+mens+premium+t-shirt-D5f1b02dd1cbf3a1b0550967b'
+  }
+];
+
+export default data;
