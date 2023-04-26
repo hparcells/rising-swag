3b93a459da7a88fd70c5fb2d4b118323a0d1d019
Hunter Parcells <hunterparcells@gmail.com>
2023-04-26 00:45:52 -0400

    Start adding PhantomRin items.

--- src/data/multiproduct/society6.ts
+++ src/data/multiproduct/society6.ts
@@ -13,6 +13,32 @@ const data: IItem[] = [
     },
     description: 'A tattered House Mars sigil and the words "House Mars" above.',
     link: 'https://society6.com/art/house-mars7056547'
+  },
+  {
+    date: '2023-04-26',
+    image:
+      'https://ctl.s6img.com/society6/img/NIogTMShceC-LtwEfhGWnRZIVtA/h_500,w_450/design/~artwork/s6-0093/a/36028932_8470398/~~/victra-wine-glass-l7f-artwork.jpg',
+    name: 'Victra (Wine Glass) Design',
+    tags: ['multiproduct', 'victra'],
+    shop: {
+      name: 'PhantomRin',
+      url: 'https://society6.com/phantomrin'
+    },
+    description: 'A lounging Victra au Julii with a wine glass.',
+    link: 'https://society6.com/art/victra-wine-glass-l7f'
+  },
+  {
+    date: '2023-04-26',
+    image:
+      'https://ctl.s6img.com/society6/img/FILc6LR9uxh2rtD2emXM76O8iMU/h_654,w_450/design/~artwork/s6-0093/a/36027649_5035224/~~/minervavirginia-70n-artwork.jpg',
+    name: 'Minerva/Virginia Design',
+    tags: ['multiproduct', 'mustang'],
+    shop: {
+      name: 'PhantomRin',
+      url: 'https://society6.com/phantomrin'
+    },
+    description: 'Virginia au Augustus with a lion behind.',
+    link: 'https://society6.com/art/minervavirginia-70n'
   }
 ];
 
