9d8ee6f1b6b50b91e6fe35568e1ff8ccf61b5efc
Hunter Parcells <hunterparcells@gmail.com>
2023-04-27 16:18:24 -0400

    Add more PhantomRin items.

--- src/data/multiproduct/society6.ts
+++ src/data/multiproduct/society6.ts
@@ -39,6 +39,123 @@ const data: IItem[] = [
     },
     description: 'Virginia au Augustus with a lion behind.',
     link: 'https://society6.com/art/minervavirginia-70n'
+  },
+  {
+    date: '2023-04-27',
+    image:
+      'https://ctl.s6img.com/society6/img/LSj2Q9gijKInpBmil1tnqMk1soU/h_654,w_450/design/~artwork/s6-0086/a/33681242_8812780/~~/red-haired-boy-oxv-artwork.jpg',
+    name: 'Red Darrow Design',
+    tags: ['multiproduct', 'darrow'],
+    shop: {
+      name: 'PhantomRin',
+      url: 'https://society6.com/phantomrin'
+    },
+    description: "A red-haired Darrow O'Lykos.",
+    link: 'https://society6.com/art/red-haired-boy-oxv'
+  },
+  {
+    date: '2023-04-27',
+    image:
+      'https://ctl.s6img.com/society6/img/h9RuQ4fxtU67uj3kbkpoJo6FHmg/h_615,w_450/design/~artwork/s6-0086/a/33681123_13453253/~~/roses-0nt-artwork.jpg',
+    name: 'Roses Design',
+    tags: ['multiproduct', 'pink'],
+    shop: {
+      name: 'PhantomRin',
+      url: 'https://society6.com/phantomrin'
+    },
+    description: 'Roses around an arm chair.',
+    link: 'https://society6.com/art/roses-0nt'
+  },
+  {
+    date: '2023-04-27',
+    image:
+      'https://ctl.s6img.com/society6/img/nd3TMbe2k5ZBK5Ho_o5dZgQ41yY/h_654,w_450/design/~artwork/s6-0086/a/33681096_11219594/~~/ares-vdk-artwork.jpg',
+    name: 'Ares Design',
+    tags: ['multiproduct', 'red'],
+    shop: {
+      name: 'PhantomRin',
+      url: 'https://society6.com/phantomrin'
+    },
+    description: 'A depiction of Ares with a helmet with a Red sigil.',
+    link: 'https://society6.com/art/ares-vdk'
+  },
+  {
+    date: '2023-04-27',
+    image:
+      'https://ctl.s6img.com/society6/img/1ecfEYQT1s_YPXAsriHmNZfjw5c/h_490,w_450/design/~artwork/s6-0085/a/33511599_10245048/~~/legacy-red-lgd-artwork.jpg',
+    name: 'Legacy Design',
+    tags: ['multiproduct', 'sevro'],
+    shop: {
+      name: 'PhantomRin',
+      url: 'https://society6.com/phantomrin'
+    },
+    description: 'Sevro in a wolf cloak looking at the helmet of Ares.',
+    link: 'https://society6.com/art/legacy-red-lgd'
+  },
+  {
+    date: '2023-04-27',
+    image:
+      'https://ctl.s6img.com/society6/img/zBiE4eHoLJUMvWu1bhQ60Jct55Q/h_654,w_450/design/~artwork/s6-0085/a/33511510_16689960/~~/old-knight-9bo-artwork.jpg',
+    name: 'Old Knight Design',
+    tags: ['multiproduct'],
+    shop: {
+      name: 'PhantomRin',
+      url: 'https://society6.com/phantomrin'
+    },
+    description: 'Lorn au Arcos.',
+    link: 'https://society6.com/art/old-knight-9bo'
+  },
+  {
+    date: '2023-04-27',
+    image:
+      'https://ctl.s6img.com/society6/img/yo-216dJw1fg6h82lxREIH1Bzog/h_627,w_450/design/~artwork/s6-0085/a/33511404_12124059/~~/beard--fox-pr7-artwork.jpg',
+    name: 'Beard & Fox Design',
+    tags: ['multiproduct', 'sophocles'],
+    shop: {
+      name: 'PhantomRin',
+      url: 'https://society6.com/phantomrin'
+    },
+    description: 'Kavax au Telemanus holding Sophocles.',
+    link: 'https://society6.com/art/beard--fox-pr7'
+  },
+  {
+    date: '2023-04-27',
+    image:
+      'https://ctl.s6img.com/society6/img/B_2cHWa-NPUknEuB_3G5v3dGsTk/h_562,w_450/design/~artwork/s6-0084/a/33171718_15934363/~~/breaking-chains-xsv-artwork.jpg',
+    name: 'Breaking Chains Design',
+    tags: ['multiproduct', 'darrow'],
+    shop: {
+      name: 'PhantomRin',
+      url: 'https://society6.com/phantomrin'
+    },
+    description: "Darrow's hands in broken chains over the color pyramid.",
+    link: 'https://society6.com/art/breaking-chains-xsv'
+  },
+  {
+    date: '2023-04-27',
+    image:
+      'https://ctl.s6img.com/society6/img/AyE5aBh0M_fxYrwrpu4qoPndNLQ/h_767,w_450/design/~artwork/s6-0084/a/33171573_6258106/~~/new-reign-artwork.jpg',
+    name: 'New Reign Design',
+    tags: ['multiproduct', 'mustang', 'gold'],
+    shop: {
+      name: 'PhantomRin',
+      url: 'https://society6.com/phantomrin'
+    },
+    description: 'Mustang and the scepter of Gold.',
+    link: 'https://society6.com/art/new-reign'
+  },
+  {
+    date: '2023-04-27',
+    image:
+      'https://ctl.s6img.com/society6/img/gAKm4SbGrSrOmXPhVLZam8AZLiw/h_654,w_450/design/~artwork/s6-0084/a/33171687_11090317/~~/moon--scars-artwork.jpg',
+    name: 'Moon & Scars Design',
+    tags: ['multiproduct'],
+    shop: {
+      name: 'PhantomRin',
+      url: 'https://society6.com/phantomrin'
+    },
+    description: 'Romulus au Raa.',
+    link: 'https://society6.com/art/moon--scars'
   }
 ];
 
