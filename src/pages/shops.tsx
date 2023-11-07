2b79427ac793e7e31d2f75db3883d76e0826a0e1
Hunter Parcells <hunterparcells@gmail.com>
2023-11-07 00:35:15 -0500

    Organized all shops page.

--- src/pages/shops.tsx
+++ src/pages/shops.tsx
@@ -27,22 +27,82 @@ function Stores() {
       <div
         className={classes.squeeze}
         style={{
-          marginTop: '1em'
+          margin: 'auto',
+          padding: '1em'
         }}
       >
         <Anchor href='/' component={Link} passHref>
           Back
         </Anchor>
+        <h1>Etsy Shops</h1>
         <ul>
-          {shops.map((shop) => {
-            return (
-              <li key={shop.name}>
-                <a href={shop.url} target='_blank'>
-                  {shop.name}
-                </a>
-              </li>
-            );
-          })}
+          {shops
+            .filter((shop) => {
+              return shop.url.includes('etsy.com');
+            })
+            .map((shop) => {
+              return (
+                <li key={shop.name}>
+                  <a href={shop.url} target='_blank'>
+                    {shop.name}
+                  </a>{' '}
+                  (
+                  {
+                    ALL_DATA.filter((item) => {
+                      return item.shop.name === shop.name;
+                    }).length
+                  }
+                  )
+                </li>
+              );
+            })}
+        </ul>
+        <h1>Redbubble Shops</h1>
+        <ul>
+          {shops
+            .filter((shop) => {
+              return shop.url.includes('redbubble.com');
+            })
+            .map((shop) => {
+              return (
+                <li key={shop.name}>
+                  <a href={shop.url} target='_blank'>
+                    {shop.name}
+                  </a>{' '}
+                  (
+                  {
+                    ALL_DATA.filter((item) => {
+                      return item.shop.name === shop.name;
+                    }).length
+                  }
+                  )
+                </li>
+              );
+            })}
+        </ul>
+
+        <h1>Other Shops</h1>
+        <ul>
+          {shops
+            .filter((shop) => {
+              return !(shop.url.includes('etsy.com') || shop.url.includes('redbubble.com'));
+            })
+            .map((shop) => {
+              return (
+                <li key={shop.name}>
+                  <a href={shop.url} target='_blank'>
+                    {shop.name}
+                  </a>{' '}
+                  (
+                  {
+                    ALL_DATA.filter((item) => {
+                      return item.shop.name === shop.name;
+                    }).length
+                  }
+                  )
+                </li>
+              );
+            })}
         </ul>
       </div>
     </Layout>
