08ce4fe502bd393db4c0e98698ee026d22989f82
Hunter Parcells <hunterparcells@gmail.com>
2023-11-09 10:04:11 -0500

    AI for title generation?

--- wizard/index.js
+++ wizard/index.js
@@ -3,6 +3,14 @@ const ncp = require('copy-paste');
 const { executablePath } = require('puppeteer'); 
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+const OpenAI = require('openai');
+
+require('dotenv').config();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+});
+
 puppeteer.use(StealthPlugin());
 
 const QUESTIONS = [
@@ -59,7 +67,7 @@ const QUESTIONS = [
         image = document.querySelector(".carousel-image").src;
         name = document.querySelector('h1').innerText;
         shopName = document.querySelector(".wt-text-link-no-underline").innerText;
-        shopUrl = `https://www.etsy.com/shop/${shopName}`
+        shopUrl = `https://www.etsy.com/shop/${shopName}`;
 
         return {
           image,
@@ -72,11 +80,22 @@ const QUESTIONS = [
       });
     }
 
+    const aiResponse = await openai.chat.completions.create({
+      messages: [
+        {
+          role: 'user',
+          content: `"${data.name}" is an name of an item on Etsy. Create a shorter title of it while trying to be shorter than 70 characters. Do not include quotes around the title. Do not mention Red Rising or Pierce Brown in the title. The end title should describe a single item. Remove any "tags". Capitalize any words that should be capitalized in the title. The part you should keep is usually at the beginning. If the title is good enough, do not change it.`
+        }
+      ],
+      model: 'gpt-3.5-turbo',
+    });
+    const name = aiResponse.choices[0].message.content;
+
     // Copy result.
     ncp.copy(`,${JSON.stringify({
       date: new Date().toISOString().split('T')[0],
       image: data.image,
-      name: data.name,
+      name,
       tags: [],
       shop: data.shop,
       description: 'FILL',
