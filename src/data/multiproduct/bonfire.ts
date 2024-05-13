3e1fd4bf94d2d8d2627ebcd37f3a805039c2e3ed
Hunter Parcells <hunterparcells@gmail.com>
2024-05-13 00:20:00 -0400

    Add Domus Luporum items.

--- src/data/multiproduct/bonfire.ts
+++ src/data/multiproduct/bonfire.ts
@@ -143,6 +143,46 @@ const data: IItem[] = [
     },
     description: 'Support The Howler Project with this shirt, hoodie, or sweatshirt!',
     link: 'https://www.bonfire.com/red-001/?productType=bacf6cd6-b53d-469c-ab96-02afe5b15f71'
+  },
+  {
+    date: '2024-05-13',
+    image:
+      'https://dynamic.bonfireassets.com/thumb/design-image/230f67d7-d433-4a28-9929-44e213e66a0d/4d2f6794-76c8-4b9e-9e78-718472227388/900/',
+    name: 'Rule #1',
+    tags: ['multiproduct'],
+    shop: {
+      name: 'Domus Luporum (The Howler Project)',
+      url: 'https://www.bonfire.com/store/domus-luporum/'
+    },
+    description: 'Rule #1: Never bow. Look into the mirror and remember.',
+    link: 'https://www.bonfire.com/rule1/'
+  },
+  {
+    date: '2024-05-13',
+    image:
+      'https://dynamic.bonfireassets.com/thumb/design-image/94ecdeeb-1bf0-4442-9262-109e49836a69/5205a469-427b-4aac-8455-556ef3ed63da/900/',
+    name: 'The 12 Days of Yuletide',
+    tags: ['multiproduct'],
+    shop: {
+      name: 'Domus Luporum (The Howler Project)',
+      url: 'https://www.bonfire.com/store/domus-luporum/'
+    },
+    description: 'Celebrate Yuletide in style with this festive design.',
+    link: 'https://www.bonfire.com/the-12-days-of-yuletide/'
+  },
+  {
+    date: '2024-05-13',
+    image:
+      'https://dynamic.bonfireassets.com/thumb/design-image/14650b2e-740a-45cb-b00b-82f5feab7b7c/5205a469-427b-4aac-8455-556ef3ed63da/900/',
+    name: 'Goblin Mode, Engage.',
+    tags: ['multiproduct'],
+    shop: {
+      name: 'Domus Luporum (The Howler Project)',
+      url: 'https://www.bonfire.com/store/domus-luporum/'
+    },
+    description:
+      'Calling all Howlers! Make Sevro proud. Check that last box and engage Goblin mode.',
+    link: 'https://www.bonfire.com/goblin-mode-engage/'
   }
 ];
 
