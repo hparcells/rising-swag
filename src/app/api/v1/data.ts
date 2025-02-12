cf6b39f618ae5211b8aed9f227f5a7205954c111
Hunter Parcells <hunterparcells@gmail.com>
2025-02-12 13:58:01 -0500

    feat: Remove API.

--- src/app/api/v1/data.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-import { ALL_DATA } from '@/data/data';
-
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.send(ALL_DATA);
-}
-export default handler;
--- src/app/api/v1/random.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-import { ALL_DATA } from '@/data/data';
-
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const randomItem = ALL_DATA[Math.floor(Math.random() * ALL_DATA.length)];
-  res.send(randomItem);
-}
-export default handler;
