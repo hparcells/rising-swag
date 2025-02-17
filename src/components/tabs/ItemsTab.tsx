25336e7772f1138dc372ea0253da1164117b7e04
Hunter Parcells <hunterparcells@gmail.com>
2025-02-17 12:51:24 -0500

    feat: Items table.

--- src/components/tabs/ItemsTab.tsx
+++ src/components/tabs/ItemsTab.tsx
@@ -1,5 +1,107 @@
+import { useEffect, useState } from 'react';
+import { ActionIcon, Anchor, Button, Image, Pagination, Table, Text } from '@mantine/core';
+import { IconPencil, IconPlus, IconTrash } from '@tabler/icons-react';
+
+import { getItems } from '@/actions/item';
+
+import { FullItem } from '@/types/item';
+
 function ItemsTab() {
-  return <p>Items Tab</p>;
+  const [items, setItems] = useState<FullItem[]>(null as any);
+  const [page, setPage] = useState(1);
+  const [pages, setPages] = useState(1);
+
+  async function fetchItems() {
+    const { total, items: fetchedItems } = await getItems(
+      {
+        search: '',
+        tags: [],
+        sort: {
+          by: 'date',
+          order: 'desc'
+        },
+        showExpired: true
+      },
+      page
+    );
+
+    setItems(fetchedItems);
+    setPages(Math.ceil(total / 30));
+  }
+
+  useEffect(() => {
+    fetchItems();
+  }, [page]);
+
+  return items ? (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: '1em 0.5em'
+      }}
+    >
+      <Button leftSection={<IconPlus size={14} />}>Add Item</Button>
+      <Table>
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th w={20}></Table.Th>
+            <Table.Th>Item</Table.Th>
+            <Table.Th>Shop</Table.Th>
+            <Table.Th>Actions</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>
+          {items.map((item) => {
+            return (
+              <Table.Tr key={item.id}>
+                <Table.Td>
+                  <Image w={30} h={30} src={item.image} alt={item.name} />
+                </Table.Td>
+                <Table.Td>
+                  <Anchor href={item.link} target='_blank'>
+                    {item.name}
+                  </Anchor>
+                </Table.Td>
+                <Table.Td>
+                  <Anchor href={item.link} target='_blank'>
+                    {item.shop.name}
+                  </Anchor>
+                </Table.Td>
+                <Table.Td>
+                  <div
+                    style={{
+                      display: 'flex',
+                      gap: '2px'
+                    }}
+                  >
+                    <ActionIcon variant='filled'>
+                      <IconPencil style={{ width: '70%', height: '70%' }} />
+                    </ActionIcon>
+                    <ActionIcon variant='filled' color='red'>
+                      <IconTrash style={{ width: '70%', height: '70%' }} />
+                    </ActionIcon>
+                  </div>
+                </Table.Td>
+              </Table.Tr>
+            );
+          })}
+        </Table.Tbody>
+      </Table>
+      <Pagination
+        total={pages}
+        value={page}
+        onChange={(newPage) => {
+          setPage(newPage);
+          // topCards.current?.scrollIntoView({ behavior: 'smooth' });
+        }}
+      />
+    </div>
+  ) : (
+    <Text m='sm'>Loading...</Text>
+  );
 }
 
 export default ItemsTab;
--- src/components/tabs/ReportsTab.tsx
+++ src/components/tabs/ReportsTab.tsx
@@ -28,65 +28,71 @@ function ReportsTab() {
     await fetchReports();
   }
 
-  return reports ? (
-    <Table>
-      <Table.Thead>
-        <Table.Tr>
-          <Table.Th w={20}></Table.Th>
-          <Table.Th>Item</Table.Th>
-          <Table.Th>Shop</Table.Th>
-          <Table.Th>Report Type</Table.Th>
-          <Table.Th>Actions</Table.Th>
-        </Table.Tr>
-      </Table.Thead>
-      <Table.Tbody>
-        {reports.map((report) => {
-          return (
-            <Table.Tr key={report.id}>
-              <Table.Td>
-                <Image w={30} h={30} src={report.item.image} alt={report.item.name} />
-              </Table.Td>
-              <Table.Td>
-                <Anchor href={report.item.link}>{report.item.name}</Anchor>
-              </Table.Td>
-              <Table.Td>
-                <Anchor href={report.item.shop.url}>{report.item.shop.name}</Anchor>
-              </Table.Td>
-              <Table.Td>{report.type}</Table.Td>
-              <Table.Td>
-                <div
-                  style={{
-                    display: 'flex',
-                    gap: '2px'
-                  }}
-                >
-                  <ActionIcon variant='filled'>
-                    <IconPencil style={{ width: '70%', height: '70%' }} />
-                  </ActionIcon>
-                  <ActionIcon
-                    variant='filled'
-                    color='red'
-                    onClick={() => {
-                      handleDelete(report.id);
-                    }}
-                  >
-                    <IconTrash style={{ width: '70%', height: '70%' }} />
-                  </ActionIcon>
-                </div>
-              </Table.Td>
-            </Table.Tr>
-          );
-        })}
-      </Table.Tbody>
-    </Table>
-  ) : (
-    <Text
+  return (
+    <div
       style={{
-        margin: '1em'
+        margin: '1em 0.5em'
       }}
     >
-      Loading...
-    </Text>
+      {reports ? (
+        <Table>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th w={20}></Table.Th>
+              <Table.Th>Item</Table.Th>
+              <Table.Th>Shop</Table.Th>
+              <Table.Th>Report Type</Table.Th>
+              <Table.Th>Actions</Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>
+            {reports.map((report) => {
+              return (
+                <Table.Tr key={report.id}>
+                  <Table.Td>
+                    <Image w={30} h={30} src={report.item.image} alt={report.item.name} />
+                  </Table.Td>
+                  <Table.Td>
+                    <Anchor href={report.item.link} target='_blank'>
+                      {report.item.name}
+                    </Anchor>
+                  </Table.Td>
+                  <Table.Td>
+                    <Anchor href={report.item.shop.url} target='_blank'>
+                      {report.item.shop.name}
+                    </Anchor>
+                  </Table.Td>
+                  <Table.Td>{report.type}</Table.Td>
+                  <Table.Td>
+                    <div
+                      style={{
+                        display: 'flex',
+                        gap: '2px'
+                      }}
+                    >
+                      <ActionIcon variant='filled'>
+                        <IconPencil style={{ width: '70%', height: '70%' }} />
+                      </ActionIcon>
+                      <ActionIcon
+                        variant='filled'
+                        color='red'
+                        onClick={() => {
+                          handleDelete(report.id);
+                        }}
+                      >
+                        <IconTrash style={{ width: '70%', height: '70%' }} />
+                      </ActionIcon>
+                    </div>
+                  </Table.Td>
+                </Table.Tr>
+              );
+            })}
+          </Table.Tbody>
+        </Table>
+      ) : (
+        <Text>Loading...</Text>
+      )}
+    </div>
   );
 }
 
