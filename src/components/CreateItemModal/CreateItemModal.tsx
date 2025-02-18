aa1beeb203bc7670b2b7dc970d5a8774d39d688c
Hunter Parcells <hunterparcells@gmail.com>
2025-02-18 11:49:24 -0500

    wip: Start item modal.

---
+++ src/components/CreateItemModal/CreateItemModal.tsx
@@ -0,0 +1,151 @@
+'use client';
+
+import { useEffect } from 'react';
+import { Button, Checkbox, Modal, Textarea, TextInput } from '@mantine/core';
+import { useForm } from '@mantine/form';
+
+import { FullItem } from '@/types/item';
+
+function CreateItemModal({
+  isOpen,
+  mode,
+  item,
+  close
+}: {
+  isOpen: boolean;
+  mode: 'create' | 'edit';
+  item: FullItem | null;
+  close: () => void;
+}) {
+  const form = useForm({
+    mode: 'uncontrolled',
+    initialValues: {
+      name: '',
+      image: '',
+      tags: '',
+      shopName: '',
+      shopUrl: '',
+      description: '',
+      link: '',
+      expired: false,
+      spoiler: false,
+      nsfw: false
+    }
+  });
+
+  function handleSubmit() {
+    form.onSubmit((values) => {
+      console.log(values);
+    });
+  }
+
+  useEffect(() => {
+    if (!isOpen) {
+      return;
+    }
+
+    form.setValues({
+      name: item?.name || '',
+      image: item?.image || '',
+      tags:
+        item?.tags
+          .map((tag) => {
+            return tag.name;
+          })
+          .join(',') || '',
+      shopName: item?.shop.name || '',
+      shopUrl: item?.shop.url || '',
+      description: item?.description || '',
+      link: item?.link || '',
+      expired: item?.expired || false,
+      spoiler: item?.spoiler || false,
+      nsfw: item?.nsfw || false
+    });
+  }, [isOpen]);
+
+  return (
+    <Modal
+      opened={isOpen}
+      onClose={close}
+      title={`${mode === 'create' ? 'Create' : 'Edit'} Item`}
+      transitionProps={{ transition: 'fade', duration: 200 }}
+    >
+      <form onSubmit={handleSubmit}>
+        <TextInput
+          withAsterisk
+          label='Name'
+          placeholder='T-Shirt'
+          key={form.key('name')}
+          {...form.getInputProps('name')}
+        />
+        <TextInput
+          withAsterisk
+          label='Image'
+          placeholder='https://...'
+          key={form.key('image')}
+          {...form.getInputProps('image')}
+        />
+        <TextInput
+          withAsterisk
+          label='Tags'
+          placeholder='t-shirt,official'
+          key={form.key('tags')}
+          {...form.getInputProps('tags')}
+        />
+        <TextInput
+          withAsterisk
+          label='Shop Name'
+          placeholder='Shop Name'
+          key={form.key('shopName')}
+          {...form.getInputProps('shopName')}
+        />
+        <TextInput
+          withAsterisk
+          label='Shop URL'
+          placeholder='https://...'
+          key={form.key('shopUrl')}
+          {...form.getInputProps('shopUrl')}
+        />
+        <Textarea
+          withAsterisk
+          label='Description'
+          placeholder='Lorem ipsum...'
+          key={form.key('description')}
+          rows={5}
+          {...form.getInputProps('description')}
+        />
+        <TextInput
+          withAsterisk
+          label='Link'
+          placeholder='https://...'
+          key={form.key('link')}
+          {...form.getInputProps('link')}
+        />
+
+        <Checkbox
+          mt='md'
+          label='Expired?'
+          key={form.key('expired')}
+          {...form.getInputProps('expired', { type: 'checkbox' })}
+        />
+        <Checkbox
+          mt='md'
+          label='Spoiler?'
+          key={form.key('spoiler')}
+          {...form.getInputProps('spoiler', { type: 'checkbox' })}
+        />
+        <Checkbox
+          mt='md'
+          label='NSFW?'
+          key={form.key('nsfw')}
+          {...form.getInputProps('nsfw', { type: 'checkbox' })}
+        />
+        <Button onClick={handleSubmit} type='submit' mt='md'>
+          Submit
+        </Button>
+      </form>
+    </Modal>
+  );
+}
+
+export default CreateItemModal;
--- src/components/tabs/ItemsTab.tsx
+++ src/components/tabs/ItemsTab.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   ActionIcon,
   Anchor,
@@ -8,118 +9,150 @@ import {
   Table,
   Text
 } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import { IconEye, IconEyeOff, IconPencil, IconPlus, IconTrash } from '@tabler/icons-react';
 
 import { toggleExpiry } from '@/actions/item';
 
 import { useFilter } from '@/hooks/filter';
 
+import { FullItem } from '@/types/item';
+
+import CreateItemModal from '../CreateItemModal/CreateItemModal';
+
 function ItemsTab() {
   const { filter, items, page, pages, isLoading, updateFilter, setPage, fetchData } = useFilter();
+
+  const [opened, { open, close }] = useDisclosure(false);
+  const [modalMode, setModalMode] = useState<'create' | 'edit'>('create');
+  const [editingItem, setEditingItem] = useState<FullItem | null>(null);
 
   async function handleExpiryClick(itemId: string) {
     await toggleExpiry(itemId);
     fetchData();
   }
 
-  return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: '1em 0.5em'
-      }}
-    >
-      <Button leftSection={<IconPlus size={14} />}>Add Item</Button>
+  function handleCreateClick() {
+    setEditingItem(null);
+    setModalMode('create');
+    open();
+  }
 
-      {!isLoading ? (
-        <>
-          <Checkbox
-            label='Show expired items'
-            checked={filter.showExpired}
-            onChange={(event) => {
-              updateFilter({ showExpired: event.currentTarget.checked });
-            }}
-            mt='sm'
-          />
-          <Table>
-            <Table.Thead>
-              <Table.Tr>
-                <Table.Th w={20}></Table.Th>
-                <Table.Th>Item</Table.Th>
-                <Table.Th>Shop</Table.Th>
-                <Table.Th w={0}>Actions</Table.Th>
-              </Table.Tr>
-            </Table.Thead>
-            <Table.Tbody>
-              {items.map((item) => {
-                return (
-                  <Table.Tr
-                    key={item.id}
-                    style={{
-                      opacity: item.expired ? 0.5 : 1
-                    }}
-                  >
-                    <Table.Td>
-                      <Image w={30} h={30} src={item.image} alt={item.name} />
-                    </Table.Td>
-                    <Table.Td>
-                      <Anchor href={item.link} target='_blank'>
-                        {item.name}
-                      </Anchor>
-                    </Table.Td>
-                    <Table.Td>
-                      <Anchor href={item.shop.url} target='_blank'>
-                        {item.shop.name}
-                      </Anchor>
-                    </Table.Td>
-                    <Table.Td>
-                      <div
-                        style={{
-                          display: 'flex',
-                          gap: '2px'
-                        }}
-                      >
-                        <ActionIcon
-                          variant='filled'
-                          onClick={() => {
-                            handleExpiryClick(item.id);
+  function handleEditClick(item: FullItem) {
+    setEditingItem(item);
+    setModalMode('edit');
+    // Fetch data.
+    open();
+  }
+
+  return (
+    <>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          margin: '1em 0.5em'
+        }}
+      >
+        <Button leftSection={<IconPlus size={14} />} onClick={handleCreateClick}>
+          Add Item
+        </Button>
+
+        {!isLoading ? (
+          <>
+            <Checkbox
+              label='Show expired items'
+              checked={filter.showExpired}
+              onChange={(event) => {
+                updateFilter({ showExpired: event.currentTarget.checked });
+              }}
+              mt='sm'
+            />
+            <Table>
+              <Table.Thead>
+                <Table.Tr>
+                  <Table.Th w={20}></Table.Th>
+                  <Table.Th>Item</Table.Th>
+                  <Table.Th>Shop</Table.Th>
+                  <Table.Th w={0}>Actions</Table.Th>
+                </Table.Tr>
+              </Table.Thead>
+              <Table.Tbody>
+                {items.map((item) => {
+                  return (
+                    <Table.Tr
+                      key={item.id}
+                      style={{
+                        opacity: item.expired ? 0.5 : 1
+                      }}
+                    >
+                      <Table.Td>
+                        <Image w={30} h={30} src={item.image} alt={item.name} />
+                      </Table.Td>
+                      <Table.Td>
+                        <Anchor href={item.link} target='_blank'>
+                          {item.name}
+                        </Anchor>
+                      </Table.Td>
+                      <Table.Td>
+                        <Anchor href={item.shop.url} target='_blank'>
+                          {item.shop.name}
+                        </Anchor>
+                      </Table.Td>
+                      <Table.Td>
+                        <div
+                          style={{
+                            display: 'flex',
+                            gap: '2px'
                           }}
                         >
-                          {item.expired ? (
-                            <IconEyeOff style={{ width: '70%', height: '70%' }} />
-                          ) : (
-                            <IconEye style={{ width: '70%', height: '70%' }} />
-                          )}
-                        </ActionIcon>
-                        <ActionIcon variant='filled'>
-                          <IconPencil style={{ width: '70%', height: '70%' }} />
-                        </ActionIcon>
-                        <ActionIcon variant='filled' color='red'>
-                          <IconTrash style={{ width: '70%', height: '70%' }} />
-                        </ActionIcon>
-                      </div>
-                    </Table.Td>
-                  </Table.Tr>
-                );
-              })}
-            </Table.Tbody>
-          </Table>
-          <Pagination
-            total={pages}
-            value={page}
-            onChange={(newPage) => {
-              setPage(newPage);
-            }}
-            mt='sm'
-          />
-        </>
-      ) : (
-        <Text mt='sm'>Loading...</Text>
-      )}
-    </div>
+                          <ActionIcon
+                            variant='filled'
+                            onClick={() => {
+                              handleExpiryClick(item.id);
+                            }}
+                          >
+                            {item.expired ? (
+                              <IconEyeOff style={{ width: '70%', height: '70%' }} />
+                            ) : (
+                              <IconEye style={{ width: '70%', height: '70%' }} />
+                            )}
+                          </ActionIcon>
+                          <ActionIcon
+                            variant='filled'
+                            onClick={() => {
+                              handleEditClick(item);
+                            }}
+                          >
+                            <IconPencil style={{ width: '70%', height: '70%' }} />
+                          </ActionIcon>
+                          <ActionIcon variant='filled' color='red'>
+                            <IconTrash style={{ width: '70%', height: '70%' }} />
+                          </ActionIcon>
+                        </div>
+                      </Table.Td>
+                    </Table.Tr>
+                  );
+                })}
+              </Table.Tbody>
+            </Table>
+            <Pagination
+              total={pages}
+              value={page}
+              onChange={(newPage) => {
+                setPage(newPage);
+              }}
+              mt='sm'
+            />
+          </>
+        ) : (
+          <Text mt='sm'>Loading...</Text>
+        )}
+      </div>
+      <CreateItemModal isOpen={opened} close={close} mode={modalMode} item={editingItem} />
+    </>
   );
 }
 
